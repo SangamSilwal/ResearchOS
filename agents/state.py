3b85b475e2a22e15df5ad97aa420274a45f1ef68
SangamSilwal <sangamsilwal2062@gmail.com>
2026-06-26 21:47:32 +0545

    chore: added architecture_design and architecture_competetion in the state graph

--- agents/state.py
+++ agents/state.py
@@ -2,12 +2,15 @@ from typing import Annotated, Any
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
 
+ 
 class ResearchState(TypedDict):
     goal: str
     messages: Annotated[list, add_messages]
     tasks: list[dict[str, Any]]
     research_findings: list[dict[str, Any]]
+    architecture_design: dict[str, Any]
+    architecture_competition: dict[str, Any]
     output: dict[str, Any]
     next_agent: str
     error: str | None
-    project_id: str |None
+    project_id: str | None
